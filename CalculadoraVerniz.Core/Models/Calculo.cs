a797848b3c7252868d70de4fba55c7f384f51e97
Manoel Américo <manoelsilva.pro.mcz@hotmail.com>
2026-05-11 17:39:45 -0300

    refact: inutilizando a class calculo, talvez seja removida futuramente.

--- CalculadoraVerniz.Core/Models/Calculo.cs
+++ CalculadoraVerniz.Core/Models/Calculo.cs
@@ -6,18 +6,18 @@ namespace CalculadoraVerniz.Core.Models
 {
     public class Calculo
     {
-        private readonly List<Medida> _medidas = new();
+        
 
-        public async Task<decimal> AreaTotal() => _medidas.Sum(c => c.Area);
+        //public async Task<decimal> AreaTotal() => _medidas.Sum(c => c.Area);
 
-        public async Task Limpar() => _medidas.Clear();
+        //public async Task Limpar() => _medidas.Clear();
 
-        public async Task Adicionar(Medida medida) => _medidas.Add(medida);
+        //public async Task Adicionar(Medida medida) => _medidas.Add(medida);
 
-        public async Task<bool> TemMedidas() => _medidas.Any();
+        //public async Task<bool> TemMedidas() => _medidas.Any();
 
-        public async Task Remover(int index) => _medidas.RemoveAt(index);
+        //public async Task Remover(int index) => _medidas.RemoveAt(index);
 
-        public async Task<IEnumerable<Medida>> Medidas() => _medidas ?? Enumerable.Empty<Medida>();
+        //public async Task<IEnumerable<Medida>> Medidas() => _medidas ?? Enumerable.Empty<Medida>();
     }
 }
