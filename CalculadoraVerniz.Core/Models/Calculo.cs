36003073f59197f14e5fc94e916a28cd670b71e3
Manoel Américo <manoelsilva.pro.mcz@hotmail.com>
2026-05-11 11:58:22 -0300

    feat: adiciona alguns metodos para adicionar medida, conferir se tem medida, remover , retornar as medidas individuais e a soma da area total.

--- CalculadoraVerniz.Core/Models/Calculo.cs
+++ CalculadoraVerniz.Core/Models/Calculo.cs
@@ -8,18 +8,16 @@ namespace CalculadoraVerniz.Core.Models
     {
         private readonly List<Medida> _medidas = new();
 
-        public async Task<decimal> AreaTotal()
-        {
-            return _medidas.Sum(c => c.Area);
-        }
+        public async Task<decimal> AreaTotal() => _medidas.Sum(c => c.Area);
 
-        public async Task Clear()
-        {
-            _medidas.Clear();
-        }
-        public async Task Add(decimal largura, decimal altura)
-        {
-            _medidas.Add(new Medida(largura, altura));
-        }
+        public async Task Clear() => _medidas.Clear();
+
+        public async Task Add(decimal largura, decimal altura) => _medidas.Add(new Medida(largura, altura));
+
+        public async Task<bool> TemMedidas() => _medidas.Any();
+
+        public async Task Remove(int index) => _medidas.RemoveAt(index);
+
+        public async Task<IEnumerable<Medida>> Medidas() => _medidas ?? Enumerable.Empty<Medida>();
     }
 }
