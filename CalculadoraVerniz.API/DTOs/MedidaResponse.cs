a3d524041ccf20a2494ed66c7fbd6b6ce1538a10
Manoel Américo <manoelsilva.pro.mcz@hotmail.com>
2026-05-11 13:03:15 -0300

    feat: adciona o Response e o Mapper para medidas.

---
+++ CalculadoraVerniz.API/DTOs/MedidaResponse.cs
@@ -0,0 +1,14 @@
+﻿namespace CalculadoraVerniz.API.DTOs
+{
+    public class MedidaResponse
+    {
+        public decimal Largura { get; set; }
+        public decimal Altura { get; set; }
+        public decimal Area { get; set; }
+
+        public MedidaResponse()
+        {
+            
+        }
+    }
+}
---
+++ CalculadoraVerniz.API/DTOs/MedidasMapper.cs
@@ -0,0 +1,16 @@
+﻿using CalculadoraVerniz.Core.Models;
+namespace CalculadoraVerniz.API.DTOs
+{
+    public class MedidasMapper
+    {  
+        public static MedidaResponse Mapper(Medida m)
+        {
+            return new MedidaResponse
+            {
+                Largura = m.Largura,
+                Altura = m.Altura,
+                Area = m.Area
+            };
+        }
+    }
+}
