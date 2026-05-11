0a5284640b8d825f56343b637df52d89191ed7bc
Manoel Américo <manoelsilva.pro.mcz@hotmail.com>
2026-05-11 17:41:03 -0300

    feat: cria um Mapper para retonar o CalculoResponseDto sem expor a entidade.

---
+++ CalculadoraVerniz.API/DTOs/CalculoMapper.cs
@@ -0,0 +1,17 @@
+﻿using CalculadoraVerniz.Core.Models;
+namespace CalculadoraVerniz.API.DTOs
+{
+    public class CalculoMapper
+    {  
+        public static CalculoResponseDto Mapper(ResultadoCalculo resultado)
+        {
+            return  new CalculoResponseDto
+            {
+                AreaTotal = resultado.AreaTotal,
+                MlTotal = resultado.MlTotal,
+                Verniz = resultado.Verniz,
+                Catalizador =  resultado.Catalizador
+            };
+        }
+    }
+}
