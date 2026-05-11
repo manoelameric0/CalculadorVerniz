33901a2b7ea30a9460118aec09fa44afd0696cf6
Manoel Américo <manoelsilva.pro.mcz@hotmail.com>
2026-05-11 13:04:04 -0300

    feat: adiciona o Controller e cria o primeiro metodo POST.

---
+++ CalculadoraVerniz.API/Controllers/VernizController.cs
@@ -0,0 +1,27 @@
+﻿using CalculadoraVerniz.API.DTOs;
+using CalculadoraVerniz.Core.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+
+namespace CalculadoraVerniz.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VernizController : ControllerBase
+    {
+        private readonly CalculoVernizService _calculadora;
+
+        public VernizController(CalculoVernizService service)
+        {
+            _calculadora = service;
+        }
+
+        [HttpPost("medida")]
+        public async Task<IActionResult> Add(MedidaRequestDto medida)
+        {
+            await _calculadora.AdicionarMedida(medida.Largura, medida.Altura);
+
+            return Created("", medida);
+        }
+    }
+}
