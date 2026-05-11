6a66c75166a6ca7b735031c5b76543cba91b39e3
Manoel Américo <manoelsilva.pro.mcz@hotmail.com>
2026-05-11 17:38:47 -0300

    refact: deixando service responsavel agora pelo calculo total. recebe uma lista contendo as medidas e o services faz o calculo restante e retorna o resulta total de cada propriedade.

--- CalculadoraVerniz.Core/Services/CalculoVernizService.cs
+++ CalculadoraVerniz.Core/Services/CalculoVernizService.cs
@@ -5,34 +5,16 @@ namespace CalculadoraVerniz.Core.Services;
 
 public class CalculoVernizService
 {
-    private readonly Calculo _calc;
-
-    public CalculoVernizService(Calculo calc)
+    public ResultadoCalculo CalcularTotais(List<Medida> medidas)
     {
-        _calc = calc;
-    }
-
-    public async Task AdicionarMedida(decimal largura, decimal altura)
-    {
-        await _calc.Adicionar(new Medida(largura, altura));
-    }
-
-    public async Task<ResultadoCalculo> CalcularTotais()
-    {
-        var areaTotal = _calc.AreaTotal().Result;
+        var areaTotal = medidas.Sum(m => m.Area);
         decimal mlTotal = areaTotal * 80;
-        decimal verniz = mlTotal * 5/6;
-        decimal catalizador = mlTotal * 1/6;
+        decimal verniz = mlTotal * 5 / 6;
+        decimal catalizador = mlTotal * 1 / 6;
 
-        await _calc.Limpar();
+        medidas.Clear();
 
         return new ResultadoCalculo(areaTotal, mlTotal, verniz, catalizador);
 
     }
-
-    public async Task<bool> TemMedidas() => await _calc.TemMedidas();
-
-    public async Task Remover(int index) => await _calc.Remover(index);
-
-    public async Task<IEnumerable<Medida>> Medidas() => await _calc.Medidas();
 }
