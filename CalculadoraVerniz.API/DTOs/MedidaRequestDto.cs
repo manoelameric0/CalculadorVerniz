8214df3ad95d8d8157e3aae99985c52cdd97dc08
Manoel Américo <manoelsilva.pro.mcz@hotmail.com>
2026-05-12 12:56:08 -0300

    feat: implementando DataAnnotations para DTO validation, para retornar Bad request por error do usuario. anteriormente retornava 500

--- CalculadoraVerniz.API/DTOs/MedidaRequestDto.cs
+++ CalculadoraVerniz.API/DTOs/MedidaRequestDto.cs
@@ -1,8 +1,13 @@
-﻿namespace CalculadoraVerniz.API.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CalculadoraVerniz.API.DTOs
 {
     public class MedidaRequestDto
     {
+        [Range(0.1, 100000)]
         public decimal Largura { get; set; }
+
+        [Range(0.1, 100000)]
         public decimal Altura { get; set; }
     }
 }
