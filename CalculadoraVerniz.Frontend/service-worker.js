c44ad348917c950fc9dbf08331e815eac4cd4b2a
Manoel Américo <manoelsilva.pro.mcz@hotmail.com>
2026-05-17 14:49:17 -0300

    create: cria o service-worker para instalação do pwa no mobile e cache para offline

---
+++ CalculadoraVerniz.Frontend/service-worker.js
@@ -0,0 +1,38 @@
+self.addEventListener("install", function(event){
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(function (cache){
+            return cache.addAll(urlsToCache);
+        })
+    )
+});
+
+self.addEventListener("activate", function (event){
+    event.waitUntil(
+        caches.keys().then(function (cacheNames){
+            return Promise.all(
+                cacheNames.map(function (cacheNames){
+                    if (cacheNames !== CACHE_NAME) {
+                        return caches.delete(cacheNames);
+                    }
+                })
+            )
+        })
+    )
+})
+
+self.addEventListener("fetch", function (event){
+    event.respondWith(
+        caches.match(event.request).then(function (response){
+            return response || fetch(event.request);
+        })
+    );
+});
+
+const CACHE_NAME = "verniz-cache-v1";
+const urlsToCache = [
+    "/",
+    "/index.html",
+    "style.css",
+    "/script.js",
+    "/manifest.json"
+];
