9b8e6cae11aa2709da2cf9569c5e433cbf939de9
Manoel Américo <manoelsilva.pro.mcz@hotmail.com>
2026-05-17 10:15:35 -0300

    update: adiciona o uso do botão calcular.
    cria um novo evetListener. async para consumir a API e retornar o json.

--- CalculadoraVerniz.Frontend/script.js
+++ CalculadoraVerniz.Frontend/script.js
@@ -1,7 +1,13 @@
 const medidas = [];
 
 const resultContent = document.getElementById("resultContent");
+const resultTitle = document.getElementById("resultTitle");
 
+
+const inputLargura = document.getElementById("inputLargura");
+const inputAltura = document.getElementById("inputAltura");
+
+const btnCalcular = document.getElementById("btnCalcular");
 const btnAdicionar = document.getElementById("btnAdicionar");
 btnAdicionar.addEventListener("click", function()
 {
@@ -30,8 +36,41 @@ btnAdicionar.addEventListener("click", function()
     
 });
 
-const inputLargura = document.getElementById("inputLargura");
-const inputAltura = document.getElementById("inputAltura");
+btnCalcular.addEventListener("click", async function() {
+    if (medidas.length === 0) {
+        alert("Adicione ao menos uma medida.");
+        return;
+    }
+
+    const response = await fetch("http://localhost:5056/api/Verniz/calcular", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            medidas: medidas
+        })
+    });
+
+    console.log(response);
+    console.log(response.status);
+
+    if (!response.ok) {
+        alert("não funfou");
+        return;
+    }
+
+    const data = await response.json();
+    console.log(data);
+
+    resultTitle.textContent = "Resultado";
+    resultContent.innerHTML = `
+        <p>Área Total: ${data.areaTotal} m²</p>
+        <p>ML total: ${data.mlTotal} ML</p>
+        <p>Verniz: ${data.verniz} ML</p>
+        <p>Catalizador: ${data.catalizador} ML</p>
+    `
+});
 
 function renderizarMedidas(){
     resultContent.innerHTML = "";
