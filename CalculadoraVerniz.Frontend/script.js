5058dc88fb21d38be413a6728b1dc73944cafd6e
Manoel Américo <manoelsilva.pro.mcz@hotmail.com>
2026-05-17 08:58:32 -0300

    update: adiciona armazenamento em memoria do frontend e ouvir o click no botão adicionar.

--- CalculadoraVerniz.Frontend/script.js
+++ CalculadoraVerniz.Frontend/script.js
@@ -1,1 +1,21 @@
-// JavaScript source code
+const medidas = [];
+
+const btnAdicionar = document.getElementById("btnAdicionar");
+btnAdicionar.addEventListener("click", function()
+{
+     console.log("clicou"),
+     console.log(inputLargura.value),
+     console.log(inputAltura.value)
+
+     const medida = {
+        largura: inputLargura.value,
+        altura: inputAltura.value
+     }
+
+     medidas.push(medida);
+     console.log(medidas);
+    
+});
+
+const inputLargura = document.getElementById("inputLargura");
+const inputAltura = document.getElementById("inputAltura");
