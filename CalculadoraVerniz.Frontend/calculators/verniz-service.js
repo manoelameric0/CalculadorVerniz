020f4c402b4b97c81cbacb3bafb2a94238c24b5d
Manoel Américo <manoelsilva.pro.mcz@hotmail.com>
2026-05-21 18:20:09 -0300

    refact: traz o fetch para o service e toda a logica da API para funcionar offline e ter um timeout.

--- CalculadoraVerniz.Frontend/calculators/verniz-service.js
+++ CalculadoraVerniz.Frontend/calculators/verniz-service.js
@@ -8,16 +8,54 @@ export class ResultadoCalculo {
 }
 
 export class VernizService {
-  CalcularTotais(medidas) {
+  async CalcularTotais(medidas) {
     let areaTotal = medidas.reduce((acc, m) => acc + (m.largura * m.altura), 0);
 
+    if (!navigator.onLine) {
+      Calcular();
+    }
 
-    let mlTotal = areaTotal * 80;
-    let verniz = mlTotal * 5 / 6;
-    let catalizador = mlTotal * 1 / 6;
+    try {
+      const controller = new AbortController();
+    const idDoTimer = setTimeout(function(){
+      controller.abort();
+    }, 8000);
 
-    medidas.length = 0;
+    const response = await fetch("https://calculadora-verniz-api.onrender.com/api/Verniz/calcular", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            medidas: medidas
+        }),
+        signal: controller.signal
+    });
 
-    return new ResultadoCalculo(areaTotal, mlTotal, verniz, catalizador);
+    // console.log(response);
+    // console.log(response.status);
+
+    // if (!response.ok) {
+    //     alert("não funfou");
+    //     return;
+    // }
+
+    const data = await response.json();
+    console.log("Calculo-API");
+    } catch (error) {
+      Calcular();
+    }
+
+    function Calcular(){
+      let mlTotal = areaTotal * 80;
+      let verniz = mlTotal * 5 / 6;
+      let catalizador = mlTotal * 1 / 6;
+
+      medidas.length = 0;
+
+      console.log("Calculo-Local");
+      return new ResultadoCalculo(areaTotal, mlTotal, verniz, catalizador);
+    }
+    
   }
 }
